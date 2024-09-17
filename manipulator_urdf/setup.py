d00fcb76fa9efb73483534853502590f1645da3d
Nytermosick <116680678+Nytermosick@users.noreply.github.com>
2024-09-17 19:42:22 +0300

    Add files via upload

---
+++ manipulator_urdf/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'manipulator_urdf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/models/', ['models/manipulator.urdf']),
+        ('share/' + package_name + '/rviz/', ['rviz/urdf.rviz']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gleb',
+    maintainer_email='stojko.g@yandex.ru',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
